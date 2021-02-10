68f6c3dfb33b0c8c1798f31dbfbc09dda1b3e615
Travis Glover <glovertravis@gmail.com>
2021-02-10 10:40:53 -0800

    credentials - 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,6 +43,16 @@ set :sidekiq_log => File.join(shared_path, 'log', 'sidekiq.log')
 
 namespace :deploy do
 
+    namespace :check do
+      before :linked_files, :set_master_key do
+        on roles(:app), in: :sequence, wait: 10 do
+          unless test("[ -f #{shared_path}/config/master.key ]")
+            upload! 'config/master.key', "#{shared_path}/config/master.key"
+          end
+        end
+      end
+    end
+
   # task :fix_absent_manifest_bug do
   #   on roles(:web) do
   #     within release_path do execute 'mkdir', release_path, 'assets_manifest_backup'
